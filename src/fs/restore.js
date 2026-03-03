52143130aeb1af9645595f008c036c8a9b8a39dc
GSerdg <grebelsergey@mail.ru>
2026-03-03 18:34:04 +0300

    feat: add restore & snapshot

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,48 @@
+import { readFile, mkdir, writeFile, access } from "node:fs/promises";
+import { join } from "node:path";
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  const BASE_JSON_PATH = join("./home/user");
+  const CURRENT_PATH = join(BASE_JSON_PATH, "workspace_restored");
+  try {
+    const snapshotJson = await readFile(
+      join(BASE_JSON_PATH, "snapshot.json"),
+    ).catch((err) => {
+      if (err.code === "ENOENT") {
+        throw new Error("FS operation failed: snapshot.json not exist");
+      }
+
+      throw err;
+    });
+    const snapshot = JSON.parse(snapshotJson);
+
+    const entriesDir = snapshot?.entries?.filter(
+      (entry) => entry.type === "directory",
+    );
+    const entriesFile = snapshot?.entries?.filter(
+      (entry) => entry.type === "file",
+    );
+
+    for (const item of entriesDir) {
+      await mkdir(join(CURRENT_PATH, item.path), { recursive: true });
+    }
+
+    for (const item of entriesFile) {
+      await writeFile(
+        join(CURRENT_PATH, item.path),
+        Buffer.from(item.content, "base64").toString("utf-8"),
+        { flag: "wx" },
+      ).catch((err) => {
+        if (err.code === "EEXIST") {
+          throw new Error("FS operation failed: file exist")
+        }
+
+        throw err;
+      });
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await restore();
--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,68 @@
+import { readdir, writeFile, stat, readFile } from "node:fs/promises";
+import { join } from "node:path";
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  const BASE_PATH = join("./home/user");
+  try {
+    
+    const userDir = await readdir(BASE_PATH, { withFileTypes: true });
+    let haveWorkspace = false;
+  
+    for (const dirent of userDir) {
+      if (dirent.isDirectory() && dirent.name === "workspace") {
+        haveWorkspace = true;
+        break;
+      }
+    }
+  
+    if (!haveWorkspace) {
+      throw new Error("FS operation failed");
+    }
+  
+    const snapshotObject = {
+      rootPath: join(BASE_PATH, "workspace"),
+      entries: [],
+    };
+    
+    const searchFiles = async (path, snapshots) => {
+      const currentPath = join(BASE_PATH, path);
+      const elements = await readdir(currentPath, { withFileTypes: true });
+  
+      for (const element of elements) {
+        if (element.isFile()) {
+          const filePath = join(element.parentPath, element.name);
+  
+          const fileStats = await stat(filePath);
+          const fileContent = await readFile(filePath);
+  
+          snapshots.entries.push({
+            path: join(element.parentPath.replace(snapshots.rootPath, ""),element.name).replaceAll('\\', '/'),
+            type: "file",
+            size: fileStats.size,
+            content: Buffer.from(fileContent).toString("base64"),
+          });
+        }
+  
+        if (element.isDirectory()) {
+          snapshots.entries.push({
+            path: join(element.parentPath.replace(snapshots.rootPath, ""), element.name).replaceAll('\\', '/'),
+            type: "directory",
+          });
+  
+          await searchFiles(join(path, element.name), snapshots);
+        }
+      }
+  
+      return snapshots;
+    };
+  
+    const snapshotObjectResult = await searchFiles("workspace", snapshotObject);
+    snapshotObjectResult.rootPath = snapshotObjectResult.rootPath.replaceAll('\\', '/');
+  
+    await writeFile(join(BASE_PATH, "snapshot.json"), JSON.stringify(snapshotObjectResult, null, 2));
+  } catch (error) {
+    console.error(error.message)
+  }
 };
 
 await snapshot();
