50fbe7dc3ae453271a0bd1ed58183f5d496d46de
GSerdg <grebelsergey@mail.ru>
2026-03-05 19:43:33 +0300

    feat: add checksums

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,36 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { pipeline } from "node:stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const verify = async () => {
-  // Write your code here
-  // Read checksums.json
-  // Calculate SHA256 hash using Streams API
-  // Print result: filename — OK/FAIL
+  const checksums = JSON.parse(
+    await readFile(join(__dirname, "checksums.json")).catch((err) => {
+      if (err.code === "ENOENT") {
+        throw new Error("FS operation failed");
+      }
+
+      throw err;
+    }),
+  );
+
+  for (const fileName in checksums) {
+    const hash = createHash("sha256");
+    const stream = createReadStream(join(__dirname, fileName));
+
+    await pipeline(stream, hash);
+
+    const finalHash = hash.digest("hex");
+
+    finalHash === checksums[fileName]
+      ? console.log(`${fileName} — OK`)
+      : console.log(`${fileName} — FAIL`);
+  }
 };
 
 await verify();
